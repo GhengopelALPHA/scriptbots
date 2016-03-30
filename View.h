8f34ae63bac25c34d8cbf7749e2844d3dd89f95a
Julian Hershey <julian.hershey@gmail.com>
2016-03-30 13:37:22 -0400

    Grabbing, bitting, crossover updates, oxygen culler, num babies, brain box successes, cell smell & random inputs, graphics update, FIXES

--- View.h
+++ View.h
@@ -5,9 +5,10 @@
 class View
 {
 public:
-    virtual void drawAgent(const Agent &a) = 0;
+    virtual void drawAgent(const Agent &a, float x, float y, bool ghost= 0) = 0;
     virtual void drawCell(int x, int y, float quantity) = 0;
     virtual void drawData() = 0;
+	virtual void drawStatic() = 0;
 };
 
 #endif // VIEW_H
